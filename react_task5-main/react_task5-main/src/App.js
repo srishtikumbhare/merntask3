a6a67196201cc975354a4a54a21e49b7b830f70d
srishtikumbhare <144722411+srishtikumbhare@users.noreply.github.com>
2023-11-10 17:33:59 +0530

    Add files via upload

---
+++ react_task5-main/react_task5-main/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import AgeCalculator from './AgeCalculator';
+import './AgeCalculator.css'
+
+function App() {
+  return (
+    <div className="App">
+      <AgeCalculator />
+    </div>
+  );
+}
+
+export default App;
